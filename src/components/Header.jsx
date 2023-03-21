bd250ea38f302018f011b892dbbd0b5e636a27c1
felipe silva <fjdsilv.dev@protonmail.com>
2023-03-21 15:46:34 -0300

    start fuzzapp

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+    <header>
+       <h1>fuzzapp</h1>
+    </header>
+)
+}
+
+export default Header
---
+++ src/components/Quizizz.jsx
@@ -0,0 +1,11 @@
+import Quiz from "./Quiz"
+
+const Quizizz = () => {
+    return (
+    <section>
+        <Quiz />
+    </section>
+    )
+}
+
+export default Quizizz
