f5c410541c9a334751df64f47f57f6bbe08a9664
felipe silva <fjdsilv.dev@protonmail.com>
2023-03-21 16:33:31 -0300

    quiz list started

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 const Header = () => {
     return (
     <header>
-       <h1>fuzzapp</h1>
+        <h1>fuzzapp</h1>
     </header>
 )
 }
--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,6 +1,47 @@
+import { useState } from "react"
+
 const Quiz = () => {
+    const [dummyData, setDummyData] = useState([
+        {
+            id: 1,
+            about: 'Football',
+            description: "lorem lorem ipsum, lorem lorem ipsum"
+        },
+        {
+            id: 2,
+            about: 'Pizza',
+            description: "lorem lorem ipsum, lorem lorem ipsum"
+        },
+        {
+            id: 3,
+            about: 'Sushi',
+            description: "lorem lorem ipsum, lorem lorem ipsum"
+        },
+        {
+            id: 4,
+            about: 'China',
+            description: "lorem lorem ipsum, lorem lorem ipsum"
+        },
+    ])
+
     return (
-    <div>Quiz</div>
+    <div>
+        <div className="quiz-list">
+            {dummyData.map(item => {
+                const { id, about, description } = item
+
+                return (
+                    <article key={id} className="quiz-item">
+                        <h2>{about}</h2>
+                        <p>{description}</p>
+                        <button>
+                            start quizz
+                        </button>
+                    </article>
+                )
+            })}
+        </div>
+    </div>
     )
 }
 
